d759449998525dcb575f2b8b50bf208e7fec112b
DefaultV <dozermailjd@gmail.com>
2022-08-22 11:20:15 +0200

    Add react-babylon basic structure

---
+++ README.md
@@ -0,0 +1,4 @@
+# Resources
+
+Babylon docs: https://doc.babylonjs.com/
+react-babylon repo: https://github.com/brianzinn/react-babylonjs
--- components/SolarSystem.tsx
+++ components/SolarSystem.tsx
@@ -1,11 +1,20 @@
 import React, { FunctionComponent } from "react";
+import { Engine, Scene } from "react-babylonjs";
+import SolarSystemObjects from "./SolarSystemObjects";
 
 interface ISolarSystemProps {}
 
 const SolarSystem: FunctionComponent<ISolarSystemProps> = () => {
   return (
-    <div>Babylon.js Solar System</div>
-  )
-}
+    <div>
+      Babylon.js Solar System
+      <Engine antialias adaptToDeviceRatio>
+        <Scene>
+          <SolarSystemObjects />
+        </Scene>
+      </Engine>
+    </div>
+  );
+};
 
-export default SolarSystem
+export default SolarSystem;
